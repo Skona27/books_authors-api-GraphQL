613b7c86124aa5fe61aacdc45c1ef112f273fe5b
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-01-10 10:33:53 +0100

    created api call from booklist component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,36 @@
 import React, {Component} from "react";
+import {gql} from 'apollo-boost';
+import {graphql} from "react-apollo";
+
+const getBooksQuery = gql`
+ {
+    books {
+        name
+        id
+    }
+ }
+`;
 
 class BookList extends Component{
+
+    getAllBooks() {
+       const data = this.props.data;
+       if(data.loading) {
+           return (<div>Loading books... </div>)
+       } else {
+           return data.books.map(book => (
+               <li key={book.id}>{book.name}</li>
+           ));
+       }
+    }
+
     render() {
         return (
             <ul>
-                <li>Book1</li>
+                {this.getAllBooks()}
             </ul>
         )
     }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
